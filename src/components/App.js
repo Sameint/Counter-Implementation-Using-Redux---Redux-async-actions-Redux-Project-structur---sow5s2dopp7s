abcb81ac6de9113cc5d612e50457ca9a3fae8a56
Ram Narayan <shukla.narayanram@gmail.com>
2023-06-21 10:00:27 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import '../styles/App.css'
+import { useDispatch,useSelector } from 'react-redux';
+import { INCREMENT, DECREMENT } from '../actions/action';
 function App() {
+const counter=useSelector((state) =>state.counter);
+const dispatch=useDispatch();
+  function increMent(){
+dispatch({type:INCREMENT})
+  }
 
+  function decreMent(){
+    dispatch({type:DECREMENT})
+  }
   return (
     <div id='main'>
-      
+      <h1 data-testid='counter'>{counter}</h1>
+      <button onClick={increMent} >+</button>
+      <button onClick={decreMent}>-</button>
     </div>
   );
 }
